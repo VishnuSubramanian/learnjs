ab01ade108ab6c53abdd6983c56622dd619f4653
Vishnu Subramanian <subramanian.kalaivanan@gmail.com>
2016-09-27 16:38:46 +0530

    second commit

---
+++ javascript/first.js
@@ -0,0 +1,4 @@
+// This is an external js file so you need to link the js file to the index.html to tell html page that it has a external js file
+console.log("Beginning of the program");
+alert("hello world");
+console.log("end of the program");
